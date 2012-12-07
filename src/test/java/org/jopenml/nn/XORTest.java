4c18bbe429283ef2e021b88a67597722b840c9d8
Felix Rodemund <frodemund@gmail.com>
2012-12-07 13:10:43 +0100

    added some visualization to XORTest

--- src/test/java/org/jopenml/nn/XORTest.java
+++ src/test/java/org/jopenml/nn/XORTest.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.jopenml.nn.Datum;
-import org.jopenml.nn.NeuralNetwork;
+import org.jopenml.nn.activationFunctions.ActivationFunction;
 import org.jopenml.nn.activationFunctions.Sigmoid;
 import org.jopenml.nn.layers.Layer;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class XORTest {
 	
-	private static final double BREAK_ON_ERROR_BELOW = .01;
-	private static final int MAX_ITERATIONS = 1000;
+	private static final double BREAK_ON_ERROR_BELOW = .001;
+	private static final int MAX_ITERATIONS = 1_000_000;
 	private static final double MOMENTUM = 0;
 	private static final double ETA = .02;
 	
@@ -47,7 +47,6 @@ public class XORTest {
 		int iterations = 0;
 		while (mlp.trainBatch(data, 1, ETA, MOMENTUM) > BREAK_ON_ERROR_BELOW) {
 			if (++iterations == MAX_ITERATIONS) {
-				System.err.println("break");
 				break;
 			}
 		}
@@ -56,14 +55,19 @@ public class XORTest {
 	private List<Layer> generateLayers() {
 		final List<Layer> layers = new ArrayList<>();
 		
+		ActivationFunction af = new Sigmoid();
+		
 		// input layer
-		layers.add(new Layer(null, 2, new Sigmoid()));
+		layers.add(new Layer(null, 2, af));
 		
 		// one hidden Layer
-		layers.add(new Layer(layers.get(0), 4, new Sigmoid()));
+		layers.add(new Layer(layers.get(0), 5, af));
+		layers.add(new Layer(layers.get(1), 5, af));
+		layers.add(new Layer(layers.get(2), 5, af));
+		layers.add(new Layer(layers.get(3), 5, af));
 		
 		// output layer
-		layers.add(new Layer(layers.get(1), 1, new Sigmoid()));
+		layers.add(new Layer(layers.get(4), 1, af));
 		return layers;
 	}
 	
@@ -79,6 +83,14 @@ public class XORTest {
 		data.add(new Datum(new double[] { 1, 1 }, target));
 		
 		return data;
+	}
+	
+	@After
+	public void printTrainingSuccess() {
+		System.err.println("target 1, value: " + mlp.classify(new double[] { 1, 0 })[0]);
+		System.err.println("target 1, value: " + mlp.classify(new double[] { 0, 1 })[0]);
+		System.err.println("target 0, value: " + mlp.classify(new double[] { 1, 1 })[0]);
+		System.err.println("target 0, value: " + mlp.classify(new double[] { 0, 0 })[0]);
 	}
 	
 }
